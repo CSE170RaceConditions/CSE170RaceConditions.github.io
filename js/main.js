7d5b8c606631830c455c5c93a3c3850d570cd21e
Kyzderp <hannah_chu@yahoo.com>
2016-10-27 23:24:49 -0700

    move avatars, finish main page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// sample code from w3, will edit later
+function move() 
+{
+  var elem = document.getElementById("myBar");
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      document.getElementById("demo").innerHTML = width * 1  + '%';
+    }
+  }
+}
