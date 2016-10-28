caf1f26ab0760ff9caed06b430c7ddbe1171d769
Kyzderp <hannah_chu@yahoo.com>
2016-10-27 20:27:16 -0700

    add index and derpy js for index

---
+++ js/userlogin.js
@@ -0,0 +1,9 @@
+function login()
+{
+	document.getElementById("loginoutput").innerHTML = "hurrdurr this isn't implemented yet";
+}
+
+function register()
+{
+	document.getElementById("registeroutput").innerHTML = "neither is this :D";
+}
