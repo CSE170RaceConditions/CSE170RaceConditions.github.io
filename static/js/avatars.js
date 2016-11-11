7feb45f9d8e3abc27cf39e11589c074765ca12bb
Kyzderp <hannah_chu@yahoo.com>
2016-11-10 23:01:48 -0800

    fix being able to click and select unlocked avatars

--- static/js/avatars.js
+++ static/js/avatars.js
@@ -1,5 +1,7 @@
 var username;
 
+var avatars;
+
 function setup()
 {
 	// Login check
@@ -12,13 +14,14 @@ function setup()
 	// Load the avatars that the user has unlocked
 	$.get("avatars/" + username, function(data)
 	{
+		avatars = data;
 		// Current selection
 		document.getElementById("selected").innerHTML = capitalizeFirst(data.selected);
 		$("#" + data.selected).css("border", "#303030 2px solid");
 
-		for (key in data)
+		for (key in data.unlocked)
 		{
-			if (data[key] == "true")
+			if (data.unlocked[key] == "true")
 			{
 				$("#" + key).children('img').attr('src', "assets/avatars/" + key + ".png");
 			}
@@ -30,13 +33,16 @@ function setup()
 	{
 		var name = this.id;
 
-		// Tell the server
-		$.get("avatars/" + username + "/" + name);
+		if (avatars.unlocked[name] == "true")
+		{
+			// Tell the server
+			$.get("avatars/" + username + "/" + name);
+			avatars.selected = name;
 
-		document.getElementById("selected").innerHTML = capitalizeFirst(name);
-
-		$(".avatar").css("border", "rgba(0, 0, 0, 0) 2px solid");
-		$("#" + name).css("border", "#303030 2px solid");
+			document.getElementById("selected").innerHTML = capitalizeFirst(name);
+			$(".avatar").css("border", "rgba(0, 0, 0, 0) 2px solid");
+			$("#" + name).css("border", "#303030 2px solid");
+		}
 	});
 }
 
