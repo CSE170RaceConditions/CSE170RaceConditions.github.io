cf0890641ef3e9b14ee4e6b0b89008c8457e23e4
Kyzderp <hannah_chu@yahoo.com>
2016-11-10 19:35:16 -0800

    login part of login page working

--- static/js/userlogin.js
+++ static/js/userlogin.js
@@ -1,9 +1,31 @@
 function login()
 {
-	document.getElementById("loginoutput").innerHTML = "hurrdurr this isn't implemented yet";
+	var username = document.getElementById("login_username").value;
+	var password = document.getElementById("login_password").value;
+
+	$.get("login/" + username + "/" + password, function(data)
+	{
+		if (data.result == "success")
+		{
+			sessionStorage.setItem("raceConditionsUsername", username);
+			console.log("Setting user: " + username)
+			window.location.href = "main";
+		}
+		else if (data.result == "noSuchUser")
+		{
+			document.getElementById("loginoutput").innerHTML = "Invalid user!";
+		}
+		else if (data.result == "wrongPassword")
+		{
+			document.getElementById("loginoutput").innerHTML = "Wrong password!";
+		}
+		else
+		{
+			document.getElementById("loginoutput").innerHTML = "Something went wrong; please try again.";
+		}
+	});
 }
 
 function register()
 {
-	document.getElementById("registeroutput").innerHTML = "neither is this :D";
 }
